d5abd33015bb10df27d71143a120340a969dc180
Viliam Kopecký <vk@viliamkopecky.cz>
2018-05-31 15:37:40 +0200

    prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,18 @@
-import * as React from "react";
-import * as styles from "./../assets/scss/App.module.sass";
+import * as React from 'react'
+import * as styles from './../assets/scss/App.module.sass'
 
-const reactLogo = require("./../assets/img/react_logo.svg");
+const reactLogo = require('./../assets/img/react_logo.svg')
 
-export interface AppProps {
-}
+export interface AppProps {}
 
 export default class App extends React.Component<AppProps, undefined> {
-    render() {
-        return (
-    <div className={styles.app}>
-                <h1>Hello World!</h1>
-                <p>Foo to the barz</p>
-                <img src={reactLogo} height="480"/>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className={styles.app}>
+				<h1>Hello World!</h1>
+				<p>Foo to the barz</p>
+				<img src={reactLogo} height="480" />
+			</div>
+		)
+	}
 }
--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,9 +1,9 @@
-declare module "*.pug" {
-  const render: (locals?: any) => string;
-  export = render;
+declare module '*.pug' {
+	const render: (locals?: any) => string
+	export = render
 }
 
-declare module "*.module.sass" {
-  const render: { [key: string]: string };
-  export = render;
+declare module '*.module.sass' {
+	const render: { [key: string]: string }
+	export = render
 }
