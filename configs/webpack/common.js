3ed6fb9609653aad7c13e5fa70dd665f82713e4a
Viliam Kopecký <vk@viliamkopecky.cz>
2018-05-31 15:49:25 +0200

    fix lint

--- configs/webpack/common.js
+++ configs/webpack/common.js
@@ -1,83 +1,83 @@
 // shared config (dev and prod)
-const { resolve } = require("path");
-const { CheckerPlugin } = require("awesome-typescript-loader");
-const StyleLintPlugin = require("stylelint-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { resolve } = require('path')
+const { CheckerPlugin } = require('awesome-typescript-loader')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".pug"]
-  },
-  context: resolve(__dirname, "../../src"),
-  module: {
-    rules: [
-      { test: /\.pug$/, loader: "pug-loader" },
-      {
-        test: /\.js$/,
-        use: ["babel-loader", "source-map-loader"],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.tsx?$/,
-        use: ["babel-loader", "awesome-typescript-loader"]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          { loader: "css-loader", options: { importLoaders: 1 } },
-          "postcss-loader"
-        ]
-      },
-      {
-        test: /\.m(odule)?\.s(c|a)ss$/,
-        loaders: [
-          "style-loader",
-          {
-            loader: "typings-for-css-modules-loader",
-            options: {
-              camelCase: true,
-              importLoaders: 2,
-              localIdentName: "[name]__[local]--[hash:base64:5]",
-              minimize: false,
-              modules: true,
-              namedExport: true,
-              sourceMap: true
-            }
-          },
-          "postcss-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.s(c|a)ss$/,
-        exclude: /\.module\.s(c|a)ss$/,
-        loaders: [
-          "style-loader",
-          { loader: "css-loader", options: { importLoaders: 1 } },
-          "postcss-loader",
-          "sass-loader"
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          "file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]",
-          "image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false"
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CheckerPlugin(),
-    new StyleLintPlugin(),
-    new HtmlWebpackPlugin({ template: "index.html.ejs" })
-  ],
-  externals: {
-    react: "React",
-    "react-dom": "ReactDOM"
-  },
-  performance: {
-    hints: false
-  }
-};
+module.exports = (stage) => ({
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx', '.pug'],
+	},
+	context: resolve(__dirname, '../../src'),
+	module: {
+		rules: [
+			{ test: /\.pug$/, loader: 'pug-loader' },
+			{
+				test: /\.js$/,
+				use: ['babel-loader', 'source-map-loader'],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.tsx?$/,
+				use: ['babel-loader', 'awesome-typescript-loader'],
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					{ loader: 'css-loader', options: { importLoaders: 1 } },
+					'postcss-loader',
+				],
+			},
+			{
+				test: /\.m(odule)?\.s(c|a)ss$/,
+				loaders: [
+					'style-loader',
+					{
+						loader: 'typings-for-css-modules-loader',
+						options: {
+							camelCase: true,
+							importLoaders: 2,
+							localIdentName: '[name]__[local]--[hash:base64:5]',
+							minimize: false,
+							modules: true,
+							namedExport: true,
+							sourceMap: true,
+						},
+					},
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.s(c|a)ss$/,
+				exclude: /\.module\.s(c|a)ss$/,
+				loaders: [
+					'style-loader',
+					{ loader: 'css-loader', options: { importLoaders: 1 } },
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: [
+					'file-loader?hash=sha512&digest=hex&name=img/[hash].[ext]',
+					'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+				],
+			},
+		],
+	},
+	plugins: [
+		new CheckerPlugin(),
+		new StyleLintPlugin(),
+		new HtmlWebpackPlugin({ template: 'index.html.ejs' }),
+	],
+	externals: {
+		react: 'React',
+		'react-dom': 'ReactDOM',
+	},
+	performance: {
+		hints: false,
+	},
+})
--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,18 +1,18 @@
 // production config
-const merge = require("webpack-merge");
-const ManifestPlugin = require("webpack-manifest-plugin");
-const { resolve } = require("path");
+const merge = require('webpack-merge')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const { resolve } = require('path')
 
-const commonConfig = require("./common");
+const commonConfig = require('./common')('production')
 
 module.exports = merge(commonConfig, {
-  mode: "production",
-  entry: "./index.tsx",
-  output: {
-    filename: "js/bundle.[hash].min.js",
-    path: resolve(__dirname, "../../dist"),
-    publicPath: "/"
-  },
-  devtool: "source-map",
-  plugins: [new ManifestPlugin({ fileName: "build.json" })]
-});
+	mode: 'production',
+	entry: './index.tsx',
+	output: {
+		filename: 'js/bundle.[hash].min.js',
+		path: resolve(__dirname, '../../dist'),
+		publicPath: '/',
+	},
+	devtool: 'source-map',
+	plugins: [new ManifestPlugin({ fileName: 'build.json' })],
+})
